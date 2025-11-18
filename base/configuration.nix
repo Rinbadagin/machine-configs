a7161a23d15e7189390499132703fd5c056199fb
Rinbadagin <klara78robinson@gmail.com>
2025-11-18 18:30:13 +1300

    Made update script agnostic & fixed disk config import in base

--- base/configuration.nix
+++ base/configuration.nix
@@ -9,7 +9,6 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
       (modulesPath + "/profiles/qemu-guest.nix")
-      ./disk-config.nix
   ];
   boot.loader.grub = {
 # no need to set devices, disko will add all devices that have a EF02 partition to the list already
