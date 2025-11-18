ca746ed60c3aaa695e2b8e3f8a23f331b4cfb52b
Rinbadagin <klara78robinson@gmail.com>
2025-11-18 22:18:25 +1300

    Add desk-friend as agent in k3s

--- desk-friend/configuration.nix
+++ desk-friend/configuration.nix
@@ -16,6 +16,12 @@
     hostName = "desk-friend";
   };
 
+  services.k3s = {
+    enable = true;
+    role = "agent";
+    tokenFile = config.age.secrets."k3s-server-token.age".path;
+    serverAddr = "https://dusty-cobweb:6443";
+  };
 
   system.stateVersion = "25.05";
 }
