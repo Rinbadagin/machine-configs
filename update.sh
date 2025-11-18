5ef3691a16b4c60af2715ad5e3c886b0bb63d49a
Rinbadagin <klara78robinson@gmail.com>
2025-11-18 18:29:17 +1300

    Made update script agnostic

--- update.sh
+++ update.sh
@@ -1,13 +1,26 @@
 #!/run/current-system/sw/bin/env zsh
 set -e
 
+if [ -z "$1" ]; then
+  echo "User@host?"
+  host=$(read -e)
+else
+  host=$1
+fi
+
+if [ -z "$2" ]; then
+  echo "Flake?"
+  flake=$(read -e)
+else
+  flake=$2
+fi
+
 echo "Commit message?"
 commit_msg=$(read -e)
-echo $commit_msg
 nix flake lock
 git add .
 git commit -m "${commit_msg}"
-nixos-rebuild switch --flake ".#deskFriend" --target-host "root@desk-friend-aertogig"
+nixos-rebuild switch --flake ".#$flake" --target-host "$host"
 
 if [ $? -eq 0 ]; then
   echo "Success. Pushing (latest: $commit_msg )"
