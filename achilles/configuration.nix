c21ec320c8f5506726c0232a00d8e2f4bc30e69e
Rinbadagin <klara78robinson@gmail.com>
2025-12-16 23:25:43 +1300

    Bring back caching for cuda :(

--- achilles/configuration.nix
+++ achilles/configuration.nix
@@ -180,7 +180,10 @@
   # services.xserver.videoDrivers = ["nvidia"];
 
   nix.settings.sandbox = true;
-
+  nix.settings = {
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
   services.xserver = {
         enable = true;
         videoDrivers = ["nvidia"];
