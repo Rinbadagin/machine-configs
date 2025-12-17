a7c08676ae9b84b861f6e21f3b0817c761add105
Rinbadagin <klara78robinson@gmail.com>
2025-12-17 22:34:46 +1300

    Added the infra

--- achilles/configuration.nix
+++ achilles/configuration.nix
@@ -33,6 +33,116 @@
     };
   };
 
+  services.lidarr = {
+    enable = true;
+  };
+
+  services.sonarr = {
+    enable = true;
+  };
+
+  services.radarr = {
+    enable = true;
+  };
+  
+  services.readarr = {
+    enable = true;
+    # settings could be populated automatically with public metadata instance here
+    # instance/settings/development setting 'Metadata Provider Source' to https://api.bookinfo.pro/
+    # https://github.com/blampe/rreading-glasses?tab=readme-ov-file
+  };
+
+  services.prowlarr = {
+    enable = true;
+  };
+
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.flood = {
+    enable = true;
+    openFirewall = true;
+    host = "0.0.0.0";
+  };
+
+  services.homepage-dashboard = {
+    enable = true;
+    openFirewall = true;
+    allowedHosts = "localhost:8082,100.64.0.9:8082,achilles:8082";
+    widgets = [
+        {
+            resources = {
+              cpu = true;
+              disk = "/";
+              memory = true;
+            };
+        }
+    ];
+    services = [
+        {
+          "desk-friend utils" = [
+            {
+              "sonarr" = {
+                description = "sonarr starr";
+                href = "http://achilles:8989";
+                widget = {
+                  type = "sonarr";
+                  url = "http://achilles:8989";
+                  key = "cc583390c7e14e59ab9dcda8c77a5e45";
+                };
+              };
+            }
+            {
+              "lidarr" = {
+                description = "lidarr starr";
+                href = "http://achilles:8686";
+                widget = {
+                  type = "lidarr";
+                  url = "http://achilles:8686";
+                  key = "04a705296c6a4d9ba2dc186c9774085d";
+                };
+              };
+            }
+            {
+              "radarr" = {
+                description = "radarr starr";
+                href = "http://achilles:7878";
+                widget = {
+                  type = "radarr";
+                  url = "http://achilles:7878";
+                  key = "cb31b22e0fd64bd7b0cbf2fffa712136";
+                };
+              };
+            }
+            {
+              "readarr" = {
+                description = "readarr starr";
+                href = "http://achilles:8787";
+                widget = {
+                  type = "readarr";
+                  url = "http://achilles:8787";
+                  key = "cb4fb2bae86b49779dc133f2444e9398";
+                };
+              };
+            }
+            {
+              "flood" = {
+                description = "flood! aah!";
+                href = "http://achilles:3000";
+                widget = {
+                  type = "flood";
+                  url = "http://achilles:3000";
+                };
+              };
+              
+            }
+          ];
+        }
+    ];
+  };
+
 #	fileSystems."/nix" =
 #	{ 
 #		depends = ["/" "/speedy" ];
