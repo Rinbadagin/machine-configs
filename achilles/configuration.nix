e4e71e6c978d6ce0f2a3f0d2d131a91677a6014a
Rinbadagin <klara78robinson@gmail.com>
2026-01-18 15:38:19 +1300

    Set user role for open-webui to admin

--- achilles/configuration.nix
+++ achilles/configuration.nix
@@ -165,15 +165,13 @@
     openFirewall = true;
     port = 11111;
     host = "0.0.0.0";
-    environment = ''
-      {
-        ANONYMIZED_TELEMETRY = "False";
-        DO_NOT_TRACK = "True";
-        SCARF_NO_ANALYTICS = "True";
-        
-        DEFAULT_USER_ROLE = "admin";
-      }
-    '';
+    environment ={
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      
+      DEFAULT_USER_ROLE = "admin";
+    };
   };
 
 #	fileSystems."/nix" =
