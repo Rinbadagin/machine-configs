be25bbe87239db5621f59fc3095b52595687a69f
Rinbadagin <klara78robinson@gmail.com>
2026-07-11 19:00:14 +1200

    Added new pubkey for avocado

--- dusty-cobweb/avocado.nix
+++ dusty-cobweb/avocado.nix
@@ -7,6 +7,9 @@
   users.users.avocado = {
     isNormalUser = true;
     openssh.authorizedKeys.keys =
-    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9S/RwneuxvHqLr+xEndd67m9pk/7lAaQ5PurQsiN+s avo@computercado" ];
+    [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9S/RwneuxvHqLr+xEndd67m9pk/7lAaQ5PurQsiN+s avo@computercado"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG6z5Jm443Z2MG2sJMSP71U/TQMshpsCe1LsCVVIAx+N avocado windows@DESKTOP-LMDC6DE"
+    ];
   };
 }
