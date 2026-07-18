eb7cb2c85184d6219794da381b4e3def8081aa7b
Rinbadagin <klara78robinson@gmail.com>
2026-07-18 20:42:35 +1200

    Moved sunshine into ghost-monitor.nix and referenced from desk-friend

--- achilles/configuration.nix
+++ achilles/configuration.nix
@@ -7,6 +7,7 @@
   imports =
     [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./ghost-monitor.nix
 # Home manager
       (import "${(builtins.fetchTarball {
   url = "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
@@ -352,47 +353,6 @@
     substituters = [ "https://aseipp-nix-cache.global.ssl.fastly.net" "https://cache.nixos-cuda.org" ];
     trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
   };
-  services.desktopManager.plasma6.enable = true;
-  
-  services.displayManager = {
-    defaultSession = "plasmax11";
-    autoLogin.enable = true;
-    autoLogin.user = "klara";
-  };
-
-  services.xserver = {
-        enable = true;
-        videoDrivers = ["nvidia"];
-        
-#        displayManager.startx.enable = true;
-        # Dummy screen
-        monitorSection = ''
-            VendorName     "Unknown"
-            HorizSync   30-85
-            VertRefresh 48-120
-
-                ModeLine        "1920x1080" 148.35  1920 2008 2052 2200 1080 1084 1089 1125
-            ModelName      "Unknown"
-            Option         "DPMS"
-        '';
-
-        deviceSection = ''
-            VendorName "NVIDIA Corporation"
-            Option      "AllowEmptyInitialConfiguration"
-            Option      "ConnectedMonitor" "DVI-D-0"
-            Option      "CustomEDID" "DFP-0"
-
-        '';
-
-        screenSection = ''
-            DefaultDepth    24
-            Option      "ModeValidation" "AllowNonEdidModes, NoVesaModes"
-            Option      "MetaModes" "1920x1080"
-            SubSection     "Display"
-                Depth       24
-            EndSubSection
-        '';
-    };
 
   hardware.nvidia = {
 
@@ -700,40 +660,6 @@
   #  };
   #};
 
-
-  security.wrappers.sunshine = {
-        owner = "root";
-        group = "root";
-        capabilities = "cap_sys_admin+p";
-        source = "${ pkgs.sunshine.override({ config.cudaSupport = true; }) }/bin/sunshine";
-    };
-
-    # Inspired from https://github.com/LizardByte/Sunshine/blob/5bca024899eff8f50e04c1723aeca25fc5e542ca/packaging/linux/sunshine.service.in
-    systemd.user.services.sunshine = {
-        description = "Sunshine server";
-        wantedBy = [ "graphical-session.target" ];
-        startLimitIntervalSec = 500;
-        startLimitBurst = 5;
-        partOf = [ "graphical-session.target" ];
-        wants = [ "graphical-session.target" ];
-        after = [ "graphical-session.target" ];
-        unitConfig.ConditionUser = "klara";
-
-        serviceConfig = let
-          sunshineConfigFile = pkgs.writeTextDir "config/sunshine.conf"
-          ''
-          origin_web_ui_allowed=wan
-          origin_pin_allowed = "wan"
-          wan_encryption_mode = 0
-          lan_encryption_mode = 0
-          ''; in {
-            ExecStart = "${config.security.wrapperDir}/sunshine ${sunshineConfigFile}/config/sunshine.conf";
-            Restart = "on-failure";
-            RestartSec = "5s";
-        };
-    };
-    
-    services.avahi.publish.userServices = true;
 
   services.joycond.enable = true;
   programs.joycond-cemuhook.enable = true;
--- desk-friend/configuration.nix
+++ desk-friend/configuration.nix
@@ -10,6 +10,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
       (modulesPath + "/profiles/qemu-guest.nix")
       ./disk-config.nix
+      ../achilles/ghost-monitor.nix
   ];
 
   networking = {
