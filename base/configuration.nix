11d03250955f074018852747bcf0387d1e3107a8
Rinbadagin <klara78robinson@gmail.com>
2026-08-12 21:20:12 +1200

    Bring back prometheus!!!

--- base/configuration.nix
+++ base/configuration.nix
@@ -114,28 +114,28 @@
   # FROM: https://wiki.nixos.org/wiki/Prometheus
   # https://nixos.org/manual/nixos/stable/#module-services-prometheus-exporters
   # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/exporters.nix
-  # services.prometheus.exporters.node = {
-  #   enable = true;
-  #   port = 9000;
-  #   # For the list of available collectors, run, depending on your install:
-  #   # - Flake-based: nix run nixpkgs#prometheus-node-exporter -- --help
-  #   # - Classic: nix-shell -p prometheus-node-exporter --run "node_exporter --help"
+  services.prometheus.exporters.node = {
+    enable = true;
+    port = 9000;
+    # For the list of available collectors, run, depending on your install:
+    # - Flake-based: nix run nixpkgs#prometheus-node-exporter -- --help
+    # - Classic: nix-shell -p prometheus-node-exporter --run "node_exporter --help"
     
-  #   enabledCollectors = [
-  #     "ethtool"   
-  #     "softirqs"
-  #     "systemd"
-  #     "tcpstat"
-  #     "diskstats"
-  #     "cpu"
-  #   ];
+    enabledCollectors = [
+      "ethtool"   
+      "softirqs"
+      "systemd"
+      "tcpstat"
+      "diskstats"
+      "cpu"
+    ];
 
-  #   # You can pass extra options to the exporter using `extraFlags`, e.g.
-  #   # to configure collectors or disable those enabled by default.
-  #   # Enabling a collector is also possible using "--collector.[name]",
-  #   # but is otherwise equivalent to using `enabledCollectors` above.
-  #   extraFlags = [ "--collector.ntp.protocol-version=4" "--no-collector.mdadm" ];
-  # };
+    # You can pass extra options to the exporter using `extraFlags`, e.g.
+    # to configure collectors or disable those enabled by default.
+    # Enabling a collector is also possible using "--collector.[name]",
+    # but is otherwise equivalent to using `enabledCollectors` above.
+    extraFlags = [ "--collector.ntp.protocol-version=4" "--no-collector.mdadm" ];
+  };
 
   system.stateVersion = "25.05";
 }
