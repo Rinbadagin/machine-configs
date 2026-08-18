9192fa2dd3747182989a4bae7873bc470f5183ee
Rinbadagin <klara78robinson@gmail.com>
2026-08-18 19:39:16 +1200

    Fixed syncthing config

--- desk-friend/configuration.nix
+++ desk-friend/configuration.nix
@@ -1,21 +1,24 @@
 {
   modulesPath,
-    lib,
-    pkgs,
-    config,
-    ...
-} @ args:
+  lib,
+  pkgs,
+  config,
+  ...
+}@args:
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-      (modulesPath + "/profiles/qemu-guest.nix")
-      ./disk-config.nix
-      ./ghost-monitor.nix
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+    ./ghost-monitor.nix
   ];
 
   networking = {
     hostName = "desk-friend";
-    nameservers = [ "9.9.9.9" "8.8.8.8" ];
+    nameservers = [
+      "9.9.9.9"
+      "8.8.8.8"
+    ];
   };
 
   services.k3s = {
@@ -35,13 +38,18 @@
 
   services.syncthing = {
     enable = true;
-    # guiAddress = "0.0.0.0:8384";
+    openDefaultPorts = true;
+    guiAddress = "0.0.0.0:8384";
+    group = "users";
+    user = "klara";
+    dataDir = "/home/klara/synced";
+    configDir = "/home/klara/.config/syncthing";
   };
 
   services.radarr = {
     enable = true;
   };
-  
+
   services.readarr = {
     enable = true;
     # settings could be populated automatically with public metadata instance here
@@ -63,7 +71,7 @@
     openFirewall = true;
     host = "0.0.0.0";
   };
-  
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
@@ -74,74 +82,74 @@
     openFirewall = true;
     allowedHosts = "localhost:8082,100.64.0.9:8082,desk-friend:8082";
     widgets = [
-        {
-            resources = {
-              cpu = true;
-              disk = "/";
-              memory = true;
-            };
-        }
+      {
+        resources = {
+          cpu = true;
+          disk = "/";
+          memory = true;
+        };
+      }
     ];
     services = [
-        {
-          "desk-friend utils" = [
-            {
-              "sonarr" = {
-                description = "sonarr starr";
-                href = "http://desk-friend:8989";
-                widget = {
-                  type = "sonarr";
-                  url = "http://desk-friend:8989";
-                  key = "cc583390c7e14e59ab9dcda8c77a5e45";
-                };
+      {
+        "desk-friend utils" = [
+          {
+            "sonarr" = {
+              description = "sonarr starr";
+              href = "http://desk-friend:8989";
+              widget = {
+                type = "sonarr";
+                url = "http://desk-friend:8989";
+                key = "cc583390c7e14e59ab9dcda8c77a5e45";
               };
-            }
-            {
-              "lidarr" = {
-                description = "lidarr starr";
-                href = "http://desk-friend:8686";
-                widget = {
-                  type = "lidarr";
-                  url = "http://desk-friend:8686";
-                  key = "04a705296c6a4d9ba2dc186c9774085d";
-                };
+            };
+          }
+          {
+            "lidarr" = {
+              description = "lidarr starr";
+              href = "http://desk-friend:8686";
+              widget = {
+                type = "lidarr";
+                url = "http://desk-friend:8686";
+                key = "04a705296c6a4d9ba2dc186c9774085d";
               };
-            }
-            {
-              "radarr" = {
-                description = "radarr starr";
-                href = "http://desk-friend:7878";
-                widget = {
-                  type = "radarr";
-                  url = "http://desk-friend:7878";
-                  key = "cb31b22e0fd64bd7b0cbf2fffa712136";
-                };
+            };
+          }
+          {
+            "radarr" = {
+              description = "radarr starr";
+              href = "http://desk-friend:7878";
+              widget = {
+                type = "radarr";
+                url = "http://desk-friend:7878";
+                key = "cb31b22e0fd64bd7b0cbf2fffa712136";
               };
-            }
-            {
-              "readarr" = {
-                description = "readarr starr";
-                href = "http://desk-friend:8787";
-                widget = {
-                  type = "readarr";
-                  url = "http://desk-friend:8787";
-                  key = "cb4fb2bae86b49779dc133f2444e9398";
-                };
+            };
+          }
+          {
+            "readarr" = {
+              description = "readarr starr";
+              href = "http://desk-friend:8787";
+              widget = {
+                type = "readarr";
+                url = "http://desk-friend:8787";
+                key = "cb4fb2bae86b49779dc133f2444e9398";
               };
-            }
-            {
-              "flood" = {
-                description = "flood! aah!";
-                href = "http://desk-friend:3000";
-                widget = {
-                  type = "flood";
-                  url = "http://desk-friend:3000";
-                };
+            };
+          }
+          {
+            "flood" = {
+              description = "flood! aah!";
+              href = "http://desk-friend:3000";
+              widget = {
+                type = "flood";
+                url = "http://desk-friend:3000";
               };
-              
-            }
-          ];
-        }
+            };
+
+          }
+        ];
+      }
     ];
   };
 
@@ -193,14 +201,16 @@
     scrapeConfigs = [
       {
         job_name = "node";
-        static_configs = [{
-          targets = [
-            "localhost:${toString config.services.prometheus.exporters.node.port}"
-            "dusty-cobweb:${toString config.services.prometheus.exporters.node.port}"
-            "achilles:${toString config.services.prometheus.exporters.node.port}"
-            "the-machine:${toString config.services.prometheus.exporters.node.port}"
+        static_configs = [
+          {
+            targets = [
+              "localhost:${toString config.services.prometheus.exporters.node.port}"
+              "dusty-cobweb:${toString config.services.prometheus.exporters.node.port}"
+              "achilles:${toString config.services.prometheus.exporters.node.port}"
+              "the-machine:${toString config.services.prometheus.exporters.node.port}"
             ];
-        }];
+          }
+        ];
       }
     ];
   };
